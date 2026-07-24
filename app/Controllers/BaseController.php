5708216608641913f9af9b4fbad62b8f5f515a17
Lemuel Velez <pluklem07@gmail.com>
2026-07-25 02:51:46 +0800

    fix: remove incompatible request property type

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -3,15 +3,12 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
-use CodeIgniter\HTTP\CLIRequest;
-use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
 abstract class BaseController extends Controller
 {
-    protected RequestInterface|IncomingRequest|CLIRequest $request;
     protected $helpers = ['cafeteria', 'form', 'url'];
     protected $session;
 
